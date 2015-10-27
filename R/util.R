6f35ba41855137a27d1f4b2ead8d1a14dae50c38
Tim Newbold <timnewbold.research@gmail.com>
2015-10-27 15:52:59 +0000

    Added a new function for changing graphical parameters before plotting

--- R/util.R
+++ R/util.R
@@ -1,3 +1,13 @@
 .Log <- function(...) {
   if(getOption('SMverbose', TRUE)) cat(...)
 }
+
+.SetPar<-function(parList){
+  
+  stopifnot(is.list(parList))
+  
+  for (p in names(parList)){
+    eval(parse(text=gsub("x",p,"par(x=parList$x)")))
+  }
+  
+}
