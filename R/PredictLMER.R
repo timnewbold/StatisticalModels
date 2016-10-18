7950b28d3a59d76982504c108114da1d91a13693
Tim Newbold <timnewbold.research@gmail.com>
2016-10-18 14:13:38 +0100

    New function for generating predicted values and confidence intervals for LMER models

---
+++ R/PredictLMER.R
@@ -0,0 +1,18 @@
+PredictLMER <- function(model,data,se.fit=FALSE,seMultiplier = 1.96){
+  
+  mm<-model.matrix(terms(model),data)
+  y <- mm %*% fixef(model)
+  
+  if (se.fit){
+    pvar1 <- diag(mm %*% base::tcrossprod(as.matrix(vcov(model)),mm))
+    yplus <- y + seMultiplier * sqrt(pvar1)
+    yminus <- y - seMultiplier * sqrt(pvar1)
+    
+    
+    return(data.frame(y=y,yplus=yplus,yminus=yminus))
+    
+  } else {
+    return(y)
+  }
+  
+}
