a073841cce542136a33f653694025b2b4edc21be
Tim Newbold <timnewbold.research@gmail.com>
2016-11-08 13:05:38 +0000

    Added function for checking overdispersion in GLMER models

---
+++ R/OverdispersionTest.R
@@ -0,0 +1,16 @@
+GLMEROverdispersion<-function(model){
+  vpars <- function(m) {
+    nrow(m)*(nrow(m)+1)/2
+  }
+  model.df <- sum(sapply(VarCorr(model),vpars))+length(fixef(model))
+  resid.df <- nrow(model.frame(model))-model.df
+  
+  resids<-residuals(model,type="pearson")
+  resid.dev<-sum(residuals(model,type="pearson")^2)
+  
+  ratio<-resid.dev/resid.df
+  
+  p<-pchisq(resid.dev,df=resid.df,lower.tail=FALSE)
+  
+  return(list(residDev=resid.dev,residDF=resid.df,ratio=ratio,P.ChiSq=p))
+}
